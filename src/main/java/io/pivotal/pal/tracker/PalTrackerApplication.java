95e93ddc18e474135b23f93ab6a94d4e447043f7
sjlee5 <sjlee5@lsis.com>
2019-08-13 09:52:00 +0900

    test

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,6 +1,5 @@
 package io.pivotal.pal.tracker;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
